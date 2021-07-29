4afd1848f6a308f4954d4d19ab59cbfdc2c63353
Atte Jokinen <45293990+AtteJ@users.noreply.github.com>
2021-07-29 12:52:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,26 @@ while True:
     r = urlopen(url)
     data_json = json.loads(r.read())
 
-    time_now = time.strftime('%Y-%m-%d %H:%M:%S')
+    time_now = time.strftime('%Y-%m-%d %H:%M:%S')  # gets system time
 
-    bpi = data_json['bpi']
-    bpi_eur = bpi['EUR']
+    bpi = data_json['bpi']  
+    bpi_eur = bpi['EUR']  # gets eur price from coindesk json response
     price_eur = bpi_eur['rate']
+    
+    bpi_usd = bpi['USD']  # gets usd price from api
+    price_usd = bpi_usd['rate']
+    
+    bpi_gbp = bpi['GBP']  # gets gbp price from api
+    price_gbp = bpi_gbp['rate']
     
     price_eur = price_eur.replace(',', '')  # removes comma and converts to float
     price_eur = float(price_eur)
+    price_usd = price_usd.replace(',', '')
+    price_usd = float(price_usd)
+    price_gbp = price_gbp.replace(',', '')
+    price_gbp = float(price_gbp)
 
-    data = [time_now, price_eur]
+    data = [time_now, price_eur]  # select currency to be added to csv
     writer.writerow(data)
 
     f.close()
