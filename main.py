2511d5656c92a93e54a3d2c9299f1edc6ce1877f
Atte Jokinen <45293990+AtteJ@users.noreply.github.com>
2021-07-29 12:43:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ while True:
     r = urlopen(url)
     data_json = json.loads(r.read())
 
-    time_data = data_json['time']
-    time_iso = time_data['updatedISO']
-    time_parsed = dateutil.parser.isoparse(time_iso)
+    time_now = time.strftime('%Y-%m-%d %H:%M:%S')
 
     bpi = data_json['bpi']
     bpi_eur = bpi['EUR']
@@ -24,11 +22,11 @@ while True:
     price_eur = price_eur.replace(',', '')  # removes comma and converts to float
     price_eur = float(price_eur)
 
-    data = [time_parsed, price_eur]
+    data = [time_now, price_eur]
     writer.writerow(data)
 
     f.close()
 
-    print('OK')  # Prints a message every time loop runs for debugging
+    print('OK', time_now)  # Prints a message every loop for debugging
 
     time.sleep(600)
